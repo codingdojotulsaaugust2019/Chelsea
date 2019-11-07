92c67dd7492293c0a3aa02d2961054ef0f828e9e
Chelsea Reynolds <chelseareynolds@Chelseas-MacBook-Air.local>
2019-11-07 08:49:39 -0600

    pushing Angular HelloWorld Assignment

--- meanStack/mongooseMongo/restfulTask/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-require('./server/config/mongoose');
-require('./server/config/routes')(app);
-
-app.listen(8000);
